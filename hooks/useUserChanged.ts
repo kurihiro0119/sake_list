36b0d9326527ed203214afb3affaaccf061756de
hikurihara <hkurihara@eviry.com>
2022-08-06 20:31:06 +0900

    first commit

---
+++ hooks/useUserChanged.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import firebase from '../firebaseConfig';
+import { useRouter } from 'next/router';
+import Cookie  from 'universal-cookie';
+
+export let unSubMeta: () => void
+
+export const useUserChanged = () => {
+    const cookie = new Cookie()
+    const router = useRouter()
+    const HASURA_TOKEN_KEY = 'https://hasura.io/jwt/claims'
+
+    useEffect(() => {
+        const unSubUser = firebase.auth().onAuthStateChanged(async (user) => {
+            if (user) {
+                const token = await user.getIdToken(true)
+                const idTOkenResult = await user.getIdTokenResult()
+                const hasuraClaims = idTOkenResult.claims[HASURA_TOKEN_KEY]
+                if (hasuraClaims) {
+                    cookie.set('token', token, { path: '/' })
+                    router.push('/tasks')
+                }
+            }
+        })
+        return () => {
+            unSubUser()
+        }
+    }, [])
+    return {}
+}
